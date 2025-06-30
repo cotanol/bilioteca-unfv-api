ea3505505e8ac629581a27ff922fae3a16f124fc
cotanol <salcedomoralesfranciscojose@gmail.com>
2025-06-30 00:26:56 -0500

    Solucionado registrar

--- src/main/java/com/unfv/biblioteca/bibliotecaapi/autenticacion/controller/AuthController.java
+++ src/main/java/com/unfv/biblioteca/bibliotecaapi/autenticacion/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("git/auth") // URL base para todo lo relacionado con autenticación
+@RequestMapping("/auth") // URL base para todo lo relacionado con autenticación
 @RequiredArgsConstructor
 public class AuthController {
 
